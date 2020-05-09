e3a733e19960903a319d3d51b9264963d27f0aad
piersdeseilligny <piers.deseilligny+github@outlook.com>
2020-05-09 13:23:23 +0100

    Add Fody to WPF

--- ProResMetadata/ProResMetadata.Wpf/Program.cs
+++ ProResMetadata/ProResMetadata.Wpf/Program.cs
@@ -14,27 +14,8 @@ namespace ProResMetadata.Wpf
 		[STAThread]
 		public static void Main(string[] args)
 		{
-            AppDomain.CurrentDomain.AssemblyResolve += OnResolveAssembly;
             new Eto.Forms.Application(Eto.Platforms.Wpf).Run(new MainForm(args, '\\'));
 
-        }
-
-        private static Assembly OnResolveAssembly(object sender, ResolveEventArgs args)
-        {
-            Assembly executingAssembly = Assembly.GetExecutingAssembly();
-            AssemblyName assemblyName = new AssemblyName(args.Name);
-
-            var path = assemblyName.Name + ".dll";
-            if (assemblyName.CultureInfo.Equals(CultureInfo.InvariantCulture) == false) path = String.Format(@"{0}\{1}", assemblyName.CultureInfo, path);
-
-            using (Stream stream = executingAssembly.GetManifestResourceStream(path))
-            {
-                if (stream == null) return null;
-
-                var assemblyRawBytes = new byte[stream.Length];
-                stream.Read(assemblyRawBytes, 0, assemblyRawBytes.Length);
-                return Assembly.Load(assemblyRawBytes);
-            }
         }
     }
 }
